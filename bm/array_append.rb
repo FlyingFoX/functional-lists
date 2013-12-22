78faabc36c6f4d425416ee770d12f7015599dfd3
thatrubylove <thatrubylove@gmail.com>
2013-12-22 10:53:37 -0800

    fixed the bm

--- bm/array_append.rb
+++ bm/array_append.rb
@@ -11,12 +11,14 @@ list  = LinkedList.new
 sample_obj = { size: 100000000 } 
 
 [100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000].each do |sample_size|
-  sample_size.times { array.push sample_obj
-                      list.push  sample_obj }
+  Benchmark.bmbm do |x|
+    x.report("array#push\t (#{sample_size})") { array.push sample_obj }
+    x.report("list#push \t (#{sample_size})") { list.push sample_obj }
+  end
 
   Benchmark.bmbm do |x|
     x.report("array#push\t (#{sample_size})") { array.push sample_obj }
-    x.report("list#push \t (#{sample_size})") { array.push sample_obj }
+    x.report("list#push \t (#{sample_size})") { list.push sample_obj }
   end
 end
 
